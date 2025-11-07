f2f12a81b78b50769f07dc7fd707323abba6ce81
Phi <nguyenxongphi@gmail.com>
2025-11-07 23:19:43 +0700

    Lầm commit trước với nội dung exception, có ý định là tách ra 2 lần commit 2 nhánh khác nhau nhưng lỡ git add . nên 2 đồng chí thông cảm :))

--- src/com/StudentManager/model/Course.java
+++ src/com/StudentManager/model/Course.java
@@ -15,7 +15,7 @@ public class StudentGroup {
 
     // Getters and Setters
     public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    public void setId(String id) { this.id = id;  }
     public String getName() { return name; }
     public void setName(String name) { this.name = name; }
     public GroupType getType() { return type; }
