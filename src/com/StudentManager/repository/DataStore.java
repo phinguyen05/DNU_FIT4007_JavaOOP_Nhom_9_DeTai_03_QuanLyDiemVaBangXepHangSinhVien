1c83fbdcacb9409a1992096910c6a51b1c66fc4c
Phuc <ducphucyeu@gmail.com>
2025-11-08 18:59:11 +0700

    Hoàn thiện Data cho chương trình.

--- src/com/StudentManager/repository/DataStore.java
+++ src/com/StudentManager/repository/DataStore.java
@@ -1,4 +1,31 @@
 package com.StudentManager.repository;
 
+import com.StudentManager.model.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataStore {
+    private static DataStore instance;
+
+    public List<Student> students;
+    public List<Lecturer> lecturers;
+    public List<Course> courses;
+    public List<Grade> grades;
+    public List<StudentGroup> studentGroups;
+
+    private DataStore() {
+        students = new ArrayList<>();
+        lecturers = new ArrayList<>();
+        courses = new ArrayList<>();
+        grades = new ArrayList<>();
+        studentGroups = new ArrayList<>();
+    }
+
+    public static synchronized DataStore getInstance() {
+        if (instance == null) {
+            instance = new DataStore();
+        }
+        return instance;
+    }
 }
