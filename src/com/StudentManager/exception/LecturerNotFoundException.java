b259d403407740633a2269fa6325702ffa73f0c6
Phi <nguyenxongphi@gmail.com>
2025-11-08 09:49:23 +0700

    Cập nhật nội dung LecturerNotFoundException.java

---
+++ src/com/StudentManager/exception/LecturerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.StudentManager.exception;
+
+public class LecturerNotFoundException extends Exception {
+    public LecturerNotFoundException(String message) {
+        super(message);
+    }
+}
